ed950be2814748eb76a40ba15df62aca6f4969bd
peng <1063629816@qq.com>
2025-01-22 15:45:48 +0800

    4

--- src/main.tsx
+++ src/main.tsx
@@ -15,12 +15,13 @@ async function enableMocking() {
 
   return new Promise(async (resolve) => {
     const {worker} = await import('./server/worker');
-
+    
     const startMockWorker = () => worker.start({
       onUnhandledRequest: 'bypass',
       quiet: false,
       serviceWorker: {
-        url: `${import.meta.env.VITE_GH_PAGES ? '/demo-dapp-with-react-ui' : ''}/mockServiceWorker.js`
+        // url: `${import.meta.env.VITE_GH_PAGES ? '/demo-dapp-with-react-ui' : ''}/mockServiceWorker.js`
+        url: `/mockServiceWorker.js`
       }
     });
     let serviceWorkerRegistration: ServiceWorkerRegistration | null | void = await startMockWorker();
